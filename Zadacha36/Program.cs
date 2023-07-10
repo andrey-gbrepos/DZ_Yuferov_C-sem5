af580ef83f4b27f8869db3df1c7fef0e24f76400
andrey-gbrepose <yandire@yandex.ru>
2023-07-10 21:22:37 +0300

    Zadacha36

--- Zadacha36/Program.cs
+++ Zadacha36/Program.cs
@@ -2,3 +2,34 @@
 //Найдите сумму элементов, стоящих на нечётных позициях.
 //[3, 7, 23, 12] -> 19
 //[-4, -6, 89, 6] -> 0
+
+Console.Clear();
+Console.Write("Введите размер массива: ");
+int sizeArr = int.Parse(Console.ReadLine());
+int[] Arr = new int[sizeArr];
+FillingAr (Arr); //Заполнение массива
+Console.WriteLine("В массиве: ");
+outArray(Arr);// Вывод массива
+Console.WriteLine($"Сумма значений чисел стоящих на нечетных позициях составляет: {sumNegativPos(Arr)}");
+
+void outArray (int[] ar)
+{                                         
+    for (int j = 0; j < ar.Length; j++) Console.Write(($"{ar[j]}, ")); 
+    
+                                        Console.WriteLine();
+}
+
+int sumNegativPos (int[] arr)
+{
+    int Sum = 0;
+    for (int i = 1; i < arr.Length; i=i+2) Sum += arr[i];
+    return Sum;
+}
+
+void FillingAr (int[] array)
+{
+    for (int i = 0; i < array.Length; i++) 
+    {
+        array[i] = new Random().Next(0,16);
+    }
+}
